ee26a65666b5cc91d5b7d3c447d3fb5830c5c09a
LuK1337 <priv.luk@gmail.com>
2020-08-10 16:12:18 -0700

    fajita: fod: Invoke callbacks when fod circle is not visible
    This reverts commit 1af53906bdcfb6c01d91c20762f871f952e619ba.
    
    Signed-off-by: AnierinB <anierinb@evolution-x.org>

--- fod/FingerprintInscreen.h
+++ fod/FingerprintInscreen.h
@@ -55,9 +55,7 @@ class FingerprintInscreen : public IFingerprintInscreen {
     Return<int32_t> getSize() override;
 
   private:
-    bool mFodCircleVisible;
     bool mIsEnrolling;
-
     sp<IOneplusDisplay> mVendorDisplayService;
     sp<IVendorFingerprintExtensions> mVendorFpService;
 
