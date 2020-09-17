a0f4abc6cb85d7f00843ab2ca17a5692fe8086f2
Anirudh Gupta <anirudhgupta109@aosip.dev>
2020-09-17 21:06:46 +0530

    fajita: Switch to enchilada build fingerprint
    Done to pass SafteyNet for the device, makes more sense to be recognized as a device with almost same everything than a random Pixel device
    
    Change-Id: I2ce0748914efe5b9cfaeff72cf6496c11059bb62
    Signed-off-by: Anirudh Gupta <anirudhgupta109@aosip.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,10 +31,6 @@ TARGET_SURFACEFLINGER_FOD_LIB := //$(DEVICE_PATH):libfod_extension.oneplus_fajit
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
 
-# Vendor init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_fajita
-TARGET_RECOVERY_DEVICE_MODULES := libinit_fajita
-
 # Prebuilt TWRP BLACK recovery
 TARGET_PREBUILT_RECOVERY_RAMDISK_CPIO := device/oneplus/fajita/twrp/ramdisk-recovery.cpio
 
